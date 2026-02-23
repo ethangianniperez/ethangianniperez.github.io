8daa3a71bb82e012192fb433474e2ae9470df2e3
ethangianniperez <ethangianniperez@gmail.com>
2026-02-23 08:28:57 -0500

    dewit

---
+++ web250/contract.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ethan Perez's Enthusiastic Penguin | WEB250 Contract</title>
+        <script src="scripts/HTMLInclude.js" defer></script>
+        <link rel="stylesheet" href="styles250/styles.css" />
+    </head>
+    <body>
+        <div data-include="components/header.html"></div>
+        <main>
+            <h2>Contract</h2>
+            <p>Welcome to the contract page for my WEB250 course. You can read more about me right<a href="introduction.html">here.</a> Thanks!</p>
+        </main>
+        <div data-include="components/footer.html"></div>
+    </body>
+</html>
