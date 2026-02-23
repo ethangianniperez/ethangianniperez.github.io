03e8f72d956ab13dbd3dfcb4a93b392de0a852ce
ethangianniperez <ethangianniperez@gmail.com>
2026-02-23 08:19:43 -0500

    components to php

---
+++ web250/components/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>WEB250</h1>
+    <nav>
+      <a href="index.html">Home</a> |
+      <a href="introduction.html">Introduction</a> |
+      <a href="contract.html">Contract</a> |
+	  
+    </nav>
+</header>
